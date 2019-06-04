82455376775337e84276af5d2e73b7de9b7b9262
Amber Jones <amberlovescats14@gmail.com>
2019-06-04 08:55:12 -0500

    finsihed with handlebars

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path')
 // const logger = require('./middleware/logger')
+const exphbs = require('express-handlebars')
+const members = require('./Members')
+
 
 const app = express()
 
@@ -9,13 +12,22 @@ const app = express()
 //everytime we make a get request it will log what is in the logger
 //* app.use(logger)
 
+//!handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
+
 //! Body parser middleware
 app.use(express.json());
-
 app.use(express.urlencoded({extended: false}))
+//*homepage route
+app.get('/', (req, res)=> res.render('index', 
+{title: "Member APP title",
+members
+}))
 
 
 //! set static
+//* The one above this is rendering because this one is below. If this one was above the hompage route, then this one would render. 
 app.use(express.static(path.join(__dirname, 'public')));
 
 //! bringing in router 
--- routes/api/members.js
+++ routes/api/members.js
@@ -34,9 +34,47 @@ router.post('/', (req, res)=> {
   } 
 
   members.push(newMember);
-  res.json(members);
+  //* for pure jason
+  // res.json(members);
+  //*or to render
+  res.redirect('/')
 
-})
+});
+
+//! update a member
+
+router.put('/:id', (req,res)=> {
+
+  const found = members.some(member => member.id === parseInt(req.params.id))
 
 
+  if(found){
+    const updMember = req.body;
+    members.forEach(member => {
+      if(member.id === parseInt(req.params.id)){
+        member.name = updMember.name ? updMember.name: member.name;
+        member.age = updMember.age ? updMember.age: member.age;
+
+        res.json({msg: "Member has been update", member: member})
+      }
+    })
+  } else {
+    res.status(400).json({msg: `No member with the id of ${req.params.id}`})
+  }
+  })
+
+  //! delete member 
+  router.delete('/:id', (req,res)=> {
+
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+
+    if(found){
+      res.json({
+        msg: "member Deleted", 
+        members: members.filter(member => member.id !== parseInt(req.params.id))})
+    } else {
+      res.status(400).json({msg: `No member with the id of ${req.params.id}`})
+    }
+    })
   module.exports = router
